0aaa98428b9814e99ff617f4a52df71c68a73ff7
Jamison <jccwinters@gmail.com>
2017-03-16 23:39:42 +0000

    changes

--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 
 const express = require('express');
 const socketIO = require('socket.io');
-var path = require('path');
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
-
+/*
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
-
+*/
 const server = express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   .use((req, res) => res.sendFile(INDEX) )
