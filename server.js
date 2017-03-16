b63257cb591725cf700865057f21932c00656223
Jamison <jccwinters@gmail.com>
2017-03-16 19:06:46 +0000

    changes

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ const socketIO = require('socket.io');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
+var mime = require('mime');
 
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
+var url = __dirname + '/css.css';
+var mimeType = mime.lookup(url);
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
+  .use((req, res) => res.setHeader('Content-Type', mimeType))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
