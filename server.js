1945f8eb3d2563cf1f0cc90036569f866c1eb3f8
Jamison <jccwinters@gmail.com>
2017-03-16 21:34:16 +0000

    changes

--- server.js
+++ server.js
@@ -9,16 +9,15 @@ const INDEX = path.join(__dirname, 'index.html');
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
+const server = express().listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
 app.get('/', function(req, res){
     res.sendFile(INDEX);
 });
 
-
-
-const server = express()
 /*
   .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  
 */
 const io = socketIO(server);
 
