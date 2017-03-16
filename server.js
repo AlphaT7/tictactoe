c609cca546d19a5c70b11f289f4263ee6d70420e
Jamison <jccwinters@gmail.com>
2017-03-16 15:30:59 +0000

    changes

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
+var app = express();
+app.use(express.static('public'))
+
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
