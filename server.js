f31dc9cd46488f037e4936bc78580db4e6332b80
Jamison <jccwinters@gmail.com>
2017-03-16 18:15:21 +0000

    changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 var app = express();
-app.use(express.static('/static'))
+app.use(express.static('static'))
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
