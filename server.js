35dab714777f1a98d29f7abb7315ed1840924da0
Jamison <jccwinters@gmail.com>
2017-03-17 08:42:11 +0000

    changes

--- server.js
+++ server.js
@@ -1,25 +1,8 @@
 var express = require('express/lib/express');
 var app = express();
-app.set('view engine', 'pug');
-app.set('views','./views');
 
 app.get('/', function(req, res){
-    res.render('index');
-});
-
-app.get('/first_template', function(req, res){
-    res.render('first_view');
-});
-
-app.get('/dynamic_view', function(req, res){
-    res.render('dynamic', {
-        name: "TutorialsPoint", 
-        url:"http://www.tutorialspoint.com"
-    });
-});
-
-app.get('/components', function(req, res){
-    res.render('content');
+    res.send('root');
 });
 
 //app.listen(process.env.PORT, process.env.IP);
