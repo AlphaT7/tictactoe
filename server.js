2a233dd40e34bc20cf2e4bf07ef0b458d8a8a2f3
Jamison <jccwinters@gmail.com>
2017-03-17 14:28:35 +0000

    changes

--- server.js
+++ server.js
@@ -1,41 +1,23 @@
-var express = require('express');  
+var express = require('express');
 const PORT = process.env.PORT || 3000;
-var app = express();  
-var server = require('http').createServer(app);  
+var app = express();
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-app.use(express.static('public'));  
-app.get('/', function(req, res,next) {  
-    res.sendFile(__dirname + 'public' + '/index.html');
+app.use(express.static('public'));
+app.get('/', function(req, res, next) {
+  res.sendFile(__dirname + 'public' + '/index.html');
 });
 
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+io.on('test', function() {
+  console.log('test successfull');
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
 server.listen(PORT);
-
-/*
-const express = require('express/lib/express');
-const socketIO = require('socket.io/lib/socket.io');
-const path = require('path');
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname + 'public', 'index.html');
-server.use(express.static('public'))
-
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const io = socketIO(server);
-
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-});
-
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
-*/
