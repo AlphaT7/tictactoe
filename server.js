8a35a1723821472acbe7cfe163819e4da237fafc
Jamison <jccwinters@gmail.com>
2017-03-17 14:16:23 +0000

    changes

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-'use strict';
+var express = require('express');  
+const PORT = process.env.PORT || 3000;
+var app = express();  
+var server = require('http').createServer(app);  
+var io = require('socket.io')(server);
 
+app.use(express.static('public'));  
+app.get('/', function(req, res,next) {  
+    res.sendFile(__dirname + 'public' + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+server.listen(PORT);
+
+/*
 const express = require('express/lib/express');
 const socketIO = require('socket.io/lib/socket.io');
 const path = require('path');
@@ -19,3 +38,4 @@ io.on('connection', (socket) => {
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+*/
