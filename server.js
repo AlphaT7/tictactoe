f8fd6a6452ab6626c3fcd82efed8d8f720b8e2c1
Jamison <jccwinters@gmail.com>
2017-03-17 08:53:49 +0000

    changes

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.get('/', function(req, res){
     res.sendFile(INDEX);
 });
 
+app.listen(process.env.PORT);
+
 var io = socketIO(app);
 
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
-
-app.listen(process.env.PORT);
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
