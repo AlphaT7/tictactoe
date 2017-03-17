d44dbcb0ec9bdf62161f234cdd2434c2d0af6e59
Jamison <jccwinters@gmail.com>
2017-03-17 08:45:41 +0000

    changes

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express/lib/express');
 var app = express();
+var INDEX = path.join(__dirname, 'index.html');
 
 app.get('/', function(req, res){
-    res.send('root');
+    res.sendFile(INDEX);
 });
 
 //app.listen(process.env.PORT, process.env.IP);
