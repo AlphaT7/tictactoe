7700394c8a524ec6e553528aef7052dd036899b4
Jamison <jccwinters@gmail.com>
2017-03-17 08:49:53 +0000

    changes

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 var path = require('path');
 var INDEX = path.join(__dirname + 'public', 'index.html');
+var socketIO = require('socket.io');
 app.use(express.static('public'))
 
 app.get('/', function(req, res){
     res.sendFile(INDEX);
 });
 
-//app.listen(process.env.PORT, process.env.IP);
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
 app.listen(process.env.PORT);
