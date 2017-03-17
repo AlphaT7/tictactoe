478f77ef33095e478c74480e146b55227c3f8565
Jamison <jccwinters@gmail.com>
2017-03-17 09:14:54 +0000

    changes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const socketIO = require('socket.io');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname + 'public', 'index.html');
+server.use(express.static('public'))
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
