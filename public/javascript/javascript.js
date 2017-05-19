3e9ee6a207eb26c6d8453ab210d5127d0ab5ea36
Jamison <jccwinters@gmail.com>
2017-05-19 20:43:13 +0000

    changes

--- public/javascript/javascript.js
+++ public/javascript/javascript.js
@@ -27,45 +27,75 @@ $('#handlebar').click(function() {
 
 });
 
-$('.tttcell').height($('.tttcell').width());
+function isEven(n) { return n % 2 == 0; }
 
-$("#createuser_wrapper").submit(function(e) {
+$('.tttcell').height($('.tttcell').width());
+/*
+$('#createuser_wrapper').submit(function(e) {
    e.preventDefault();
    if ($('#username').val() != '') {
       $.ajax({
-            method: "GET",
-            url: "/userid",
+            method: 'GET',
+            url: '/userid',
             data: {
                name: $('#username').val()
             }
          })
          .done(function(data) {
-            username = data;
-            alert(data);
+            if(data == 'success'){
+               username = $('#username').val();
+               alert('Your username is: ' + username);
+               $('#createuser_wrapper').css('display', 'none');
+               $('#uid_info').html('User Name: ' + username);
+            } else {
+               alert(data);
+            }
          });
    }
 });
+*/
+$('#createuser_wrapper').submit(function(e) {
+   e.preventDefault();
+   if ($('#username').val() != '') {
+      socket.emit('createuser', $('#username').val());
+   }
+});
 
-$("#creategame_wrapper").submit(function(e) {
+$('#creategame_wrapper').submit(function(e) {
    e.preventDefault();
    if ($('#gamename').val() != '' && username != '') {
-      $.ajax({
-            method: "GET",
-            url: "/gamename",
-            data: {
-               name: $('#gamename').val()
-            }
-         })
-         .done(function(room) {
-            alert(room);
-            gameroom = room;
-            $('#gamelist').html($('#gamelist').html() + '<option id="' + gameroom + '"> ' + gameroom + ' </option>')
-            console.log('You have joined: ' + gameroom);
-            socket.emit('joinroom', gameroom);
-         });
+      if($('#gname_info').html() != ''){
+         var r = confirm("Are you sure you wish to leave your existing game: " + gameroom);
+         if(r){
+            socket.emit('creategame', {'gamename': $('#gamename').val(), 'oldgamename':gameroom, 'username': username});
+            $('#gname_info').html('Game Name: ' + $('#gamename').val() + ' ');
+            playerturn = 1;
+         }
+      } else {
+         socket.emit('creategame', {'gamename': $('#gamename').val(), 'username': username});
+         $('#gname_info').html('Game Name: ' + $('#gamename').val() + ' ');
+         playerturn = 1;         
+      }
    }
 });
 
-$('#text_btn').click(function(){
-   socket.emit('test',gameroom);
+$('#joingame_wrapper').submit(function(e) {
+   e.preventDefault();
+   if ($('#gamelist').val() != 'default' && username != '') {
+      if($('#gname_info').html() != ''){
+      var r = confirm("Are you sure you wish to leave your existing game: " + gameroom);
+         if(r){
+            socket.emit('joingame', {'gamename': $('#gamelist').val(), 'oldgamename':gameroom, 'username': username});
+            playerturn = 2;            
+         }
+      } else {
+         socket.emit('joingame', {'gamename': $('#gamelist').val(), 'username': username});
+         playerturn = 2;
+      }
+   }
+});
+
+$('.tttcell').click(function() {
+    var cid = this.id;
+    socket.emit('cellclick', {'gameroom': gameroom, 'cellid': cid, 'playerturn': playerturn});
 });
--- server.js
+++ server.js
@@ -1,4 +1,50 @@
 // https://gist.github.com/crtr0/2896891 --> Socket.IO Rooms Example
+// io.emit --> broadcast to everyone
+// socket.emit --> broadcast to just the sender
+
+/*
+// sending to sender-client only
+socket.emit('message', "this is a test");
+
+// sending to all clients, include sender
+io.emit('message', "this is a test");
+
+// sending to all clients except sender
+socket.broadcast.emit('message', "this is a test");
+
+// sending to all clients in 'game' room(channel) except sender
+socket.broadcast.to('game').emit('message', 'nice game');
+
+// sending to all clients in 'game' room(channel), include sender
+io.in('game').emit('message', 'cool game');
+
+// sending to sender client, only if they are in 'game' room(channel)
+socket.to('game').emit('message', 'enjoy the game');
+
+// sending to all clients in namespace 'myNamespace', include sender
+io.of('myNamespace').emit('message', 'gg');
+
+// sending to individual socketid
+socket.broadcast.to(socketid).emit('message', 'for your eyes only');
+*/
+
+
+// Player Turn notification ------------------------------ done!
+// Auto-resizign x/o boxes ------------------------------- done!
+// Prevent over-write of existing coordinates ------------ done!
+// Make sure loser gets a message ------------------------ done!
+// Change favico ----------------------------------------- done!
+// Remove un-neccessary modules -------------------------- done!
+// Fix the tie notice ------------------------------------ done!
+// Add neon glow to winning streak and losing side ------- done!
+// Fix winning combinations bug -------------------------- done!
+// Make sure it doesn't broadcast gamerooms that are full  done!
+// New Game button, and notice if you switch gamerooms
+// Backup all the tictacto code locally
+// Change heroku sub-domain / prefix
+// Upload final version to heroku
+
+
 
 var express = require('express');
 var PORT = process.env.PORT || 3000;
@@ -6,113 +52,219 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-var users = [];
-var games = [];
+var users = {};
+var games = {};
+var winningcombinations = [
+  ['1.1','2.1','3.1'],
+  ['1.2','2.2','3.2'],
+  ['1.3','2.3','3.3'],
+  
+  ['1.1','1.2','1.3'],
+  ['2.1','2.2','3.2'],
+  ['3.1','3.2','3.3'],
+  
+  ['1.1','2.2','3.3'],
+  ['1.3','2.2','3.1']
+]
 
+function isEven(n) { return n % 2 == 0; }
+
+function gameOver(playerarray){
+  var strike = 0;
+  for (var j = 0; j < winningcombinations.length; j++){
+    for (var i = 0; i < playerarray.length; i ++){
+      if(winningcombinations[j].indexOf(playerarray[i]) != -1){
+        strike++;
+      }
+    }
+    if (strike == 3){ return winningcombinations[j]; } else { strike = 0; }
+  }
+  return false;
+}
+
+function gameroomcleanup(data, socket){
+  console.log('oldgamename - ' + data.oldgamename);
+  if(data.oldgamename != undefined){
+    games[data.oldgamename].gameover = true;
+    socket.broadcast.to(data.oldgamename).emit('message', 'Your opponent has left the game.');
+    socket.leave(data.oldgamename);
+  }
+}
+
+/*
+function searchObj (obj, query) {
+    for (var key in obj) {
+        var value = obj[key];
+        if (typeof value === 'object') {
+            searchObj(value, query);
+        }
+        if (value === query) {
+            console.log('property=' + key + ' value=' + value);
+        }
+    }
+}
+*/
 app.use(express.static('public'));
 
 app.get('/', function(req, res, next) {
   res.sendFile(__dirname + 'public' + '/index.html');
 });
 
-app.get('/userid', function(req, res, next) {
-
-  var name = req.query.name;
-
-  if (users.indexOf(name) != '-1') {
-    res.send('User Exists');
-  } else {
-    users.push(name);
-    res.send(name);
-  }
-
-});
-
-app.get('/gamename', function(req, res, next) {
-
-  var gameroom = req.query.name;
-
-  if (games.indexOf(gameroom) != '-1') {
-    res.send('Game Exists');
-  } else {
-    games.push(gameroom);
-    res.send(gameroom);
-  }
-
-});
-
-
-
 io.on('connection', (socket) => {
   console.log('client connected');
   
-  var rooms = io.sockets.adapter.rooms;
-  
-  /*
-    var numClients = {};
+  //socket.id;
+  //socket.adapter.rooms;
+  //var rooms = io.sockets.adapter.rooms;
 
-    socket.on('join', function(room) {
-      socket.join(room);
-      socket.room = room;
-      if (numClients[room] == undefined) {
-        numClients[room] = 1;
-      } else {
-        numClients[room]++;
+// Check if each room is full
+// If it isn't, then broadcast the updated object of opengames to the new player
+  if(Object.keys(games).length !== 0){
+    var opengames = {};
+    for(var gamename in games){
+      //var obj2 = JSON.parse(JSON.stringify(obj1));
+      if(games[gamename].playercount < 2){
+        opengames[gamename] = JSON.parse(JSON.stringify(games[gamename]));
       }
-      socket.emit('test',numClients[socket.room].length)
-    });
-  */
+    }
+    
+    socket.emit('addgamefirstload', opengames);
+  }
 
   socket.on('cellclick', function(data) {
-    io.emit('clickresponse', data);
-    console.log(data);
-  });
-
-  socket.on('clickresponse', function(data) {
-    console.log(data);
-  });
-
-  socket.on('room', function(room) {
-    socket.join(room);
-  });
-
-  socket.on('joinroom', function(room) {
-    socket.join(room, function() {
-      console.log(socket.rooms); // [ <socket.id>, 'room 237' ]
-    });
-  });
-
-  socket.on('test', function() {
-    io.emit('test_response', 'Test was successfull!')
-/*
-    console.log(socket.room);
-    console.log(rooms)
-*/
-    console.log((Object.keys(rooms).length));
-  })
-
-
-  /*
-    socket.on('uid', function(data){
-      if(users.indexOf(data) != '-1'){
-        io.emit('uid_check', {response: 'User Exists'});
+    if(data.gameroom == '' || data.cellid  == '' || data.playerturn  == '') { 
+      socket.emit('message','Please setup your game before entering a coordinate!'); 
+    } else {
+      
+      if(games[data.gameroom].gameover == false){
+        if(isEven(data.playerturn) == isEven(games[data.gameroom].playerturn)){
+          if(games[data.gameroom].usedmoves.indexOf(data.cellid) == -1){
+            games[data.gameroom].usedmoves.push(data.cellid);
+            games[data.gameroom][socket.id].push(data.cellid);
+            io.in(data.gameroom).emit('clickresponse', {'cellid': data.cellid, 'playerturn': games[data.gameroom].playerturn});
+            if(gameOver(games[data.gameroom][socket.id]) != false){
+              socket.emit('message', "Victory!");
+              io.in(data.gameroom).emit('winstreak',{'coordsarray': gameOver(games[data.gameroom][socket.id]),'playerturn': games[data.gameroom].playerturn});
+              socket.broadcast.to(data.gameroom).emit('message', 'You have lost! Better luck next time. =)');
+            } else if(gameOver(games[data.gameroom][socket.id]) == false && games[data.gameroom].playerturn == 9){
+              io.in(data.gameroom).emit('message', "Game Over! You have a Tie!") 
+            }
+            games[data.gameroom].playerturn++;
+            
+            if(isEven(games[data.gameroom].playerturn)){
+              io.in(data.gameroom).emit('playerturn', 2);
+            } else {
+              io.in(data.gameroom).emit('playerturn', 1);
+            }
+          
+          } else {
+            socket.emit('message', "This coordinate has already been used!");
+          }    
+              
+        } else {
+          socket.emit('message', 'Please wait your turn! =)')
+        }
       } else {
-        users.push(data);
-        io.emit('uid_check', {response: 'User Created', uid: data});
+        socket.emit('message', 'Game Over. Please start or join a new game!')
       }
-    });
-  */
+      
+    }
+  });
+
+/*
+  games = {
+    'gameroom-name': {
+      gameover: false,
+      socket.id: [], // player1 moves
+      socket.id: [], // player2 moves
+      usedmoves: [],
+      player1: 'player1 name',
+      player2: 'player2 name',
+      playerturn: 1-9,
+      playercount: '1-2'
+    }
+  }
+*/
+
+  socket.on('createuser', function(data){
+    var name = data;
+    if(users.hasOwnProperty(name)){
+      socket.emit('message','This user name is already in use.');
+    } else {
+      users[name] = socket.id;
+      socket.emit('create_user', name);
+      console.log('Username: ' + name + ' has been assigned.');
+    }
+  });
 
 
+  socket.on('creategame', function(data){
+    if(games.hasOwnProperty(data.gamename)){
+      socket.emit('message', 'This name is already in use.')
+    } else {
+      gameroomcleanup(data, socket);
+      socket.join(data.gamename, function() {
+        games[data.gamename] = {'playercount': 1, 'playerturn': 1, [socket.id]: [], 'player1': data.username, 'usedmoves': [], 'gameover': false};
+        socket.emit('joingame',data.gamename);
+        socket.broadcast.emit('addgame', data.gamename);
+        console.log('Game Created: ' + data.gamename + ' has been created.');
+      });
+    }
+  });
+  
+  socket.on('joingame', function(data) {
+    
+    if(games[data.gamename].playercount < 2){
+        gameroomcleanup(data, socket);
+        socket.join(data.gamename, function() {
+          games[data.gamename][socket.id] = [];
+          games[data.gamename].player2 = data.username;
+          games[data.gamename].playercount ++;
+          socket.emit('message', 'You have joined the ' + data.gamename + ' game room.');
+          io.emit('removegame',data.gamename);
+          socket.emit('joingame',data.gamename);
+          setTimeout(function(){socket.broadcast.to(data.gamename).emit('opponentname', data.username)},375);
+          io.in(data.gamename).emit('playerturn', 1);
+      });      
+    } else {
+      socket.emit('message', 'This game room is full.');
+    }
+    
+  });
+  
+  socket.on('serveropponentname', function(data){
+    socket.broadcast.to(data.gamename).emit('opponentname2', data.username);
+  });
 
   socket.on('error', function(err) {
     console.log(err);
   });
 
   socket.on('disconnect', function() {
+    
+    for (var user in users){
+      if (users[user] == socket.id){
+        delete users[user];
+        break;
+      }
+    }
+    
+    for (var gamename in games){
+      if(socket.adapter.rooms.hasOwnProperty(gamename) != true){
+        // if it doesn't exist, then delete it from the games object and update the gameslist select/options
+        delete games[gamename];
+        io.emit('removegame',gamename);
+      }
+      
+      for (var gameproperty in games[gamename]){
+        if (gameproperty.toString() == socket.id.toString()){
+          socket.broadcast.to(gamename).emit('message', 'Your opponent has left the game. Game Over.');
+          break;
+        }
+      }
+    }
     console.log('client disconnected')
   });
-
 
 });
 
